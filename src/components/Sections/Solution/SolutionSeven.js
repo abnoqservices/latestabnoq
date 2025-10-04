5d85e9aca9a71f1631aec12c24d8ed500b994ff5
abnoqservices <95683430+abnoqservices@users.noreply.github.com>
2025-10-04 15:06:04 +0530

    Update SolutionSeven.js

--- src/components/Sections/Solution/SolutionSeven.js
+++ src/components/Sections/Solution/SolutionSeven.js
@@ -5,7 +5,7 @@ const SolutionSeven = ({ data, start, limit }) => {
         <section className="section-solution bg-white lg:py-20 sm:py-14 py-10">
             <div className="container text-center">
                 <span className="tag text-label bg-blue text-white">How we do</span>
-                <h3 className="heading3 text-center mt-3">Our solutions</h3>
+                <div className="heading3 text-center mt-3">Our solutions</div>
                 <div className="grid lg:grid-cols-3 sm:grid-cols-2 lg:gap-[30px] gap-5 md:mt-10 mt-6">
                     {data.slice(start, limit).map((item, index) => (
                         <ServiceItem data={item} key={index} type={'style-seven'} />
@@ -15,4 +15,5 @@ const SolutionSeven = ({ data, start, limit }) => {
         </section>
     )
 }
+
 export default SolutionSeven
